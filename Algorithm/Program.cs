12c20d4a28e70bf8038b3f7f0ec75f79c61a700d
GwanYoungJO <whaudgnl0131@naver.com>
2023-04-17 17:05:22 +0900

    first commit

---
+++ Algorithm/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int lasttick = 0;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            while (true)
+            {
+                #region 프레임 관리
+                int currenTick = System.Environment.TickCount;
+                int elapsedTick = currenTick - lasttick;
+
+                // 만약에 경과한 시간이 1/30초보다 작다면
+                if (elapsedTick < WAIT_TICK)
+                    continue;
+                lasttick = currenTick;
+                #endregion 
+                // FPS 프레임 (60프레임 OK / 30 프레임 이하로 NO)
+                // 입력
+
+                // 로직
+
+                // 렌더링 
+                Console.SetCursorPosition(0, 0);
+
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
