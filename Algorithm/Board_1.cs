6fcb11c3f1b33ebd09b199fd8f1c178b099b6f41
GwanYoungJO <whaudgnl0131@naver.com>
2023-04-17 18:27:50 +0900

    first commit

---
+++ Algorithm/Board_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Room
+    {
+        Room next;
+        //public 
+    }
+
+    class Board_1   //연결리스트 구현 연습
+    {
+        public List<int> _data2 = new List<int>();  // 동적배열   // 재너릴 형식
+        public LinkedList<int> _data3 = new LinkedList<int>();  //연결 리스트
+
+        public void Initialize()
+        {
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            LinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
+
+            _data3.Remove(node);
+        }
+    }
+}
